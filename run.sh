745fa6705452786c76dcf967f0e7ff7de27bf958
Andreas Linz <klingt.net@gmail.com>
2014-09-18 12:39:29 +0200

    changed the output to file to a user specific name

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+OUTFILE="/usr/share/gnome-background-properties/${USER}_wallpapers.xml"
+
 if [ -d "$1" ]; then
-    echo "output will be redirected to /usr/share/gnome-background-properties/user_wallpapers.xml"
-    src/main.py $1 > /usr/share/gnome-background-properties/user_wallpapers.xml
+    echo "output will be redirected to $OUTFILE"
+    src/main.py $1 > $OUTFILE
 else
     echo "usage: ./run.sh wallpaper_path"
 fi
